7c690be6b1df7994fb4b373101775f91be102424
Jacob <312846421@qq.com>
2017-12-21 17:46:05 +0800

    init projec

---
+++ JAFrameDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JAFrameDemo
+//
+//  Created by Jacob on 2017/12/21.
+//  Copyright © 2017年 Jacob_LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JAFrameDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JAFrameDemo
+//
+//  Created by Jacob on 2017/12/21.
+//  Copyright © 2017年 Jacob_LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
